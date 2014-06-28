a43f6798ffabab7477cdacb31dd407ab41778754
Jonathan Nafta <yoninafta@gmail.com>
2014-06-28 21:35:37 +0300

    update to python camera module

--- camera_scripts.py
+++ camera_scripts.py
@@ -3,15 +3,18 @@ import pygame.camera
 
 pygame.camera.init()
 
-
+def webcam_take_photo(file_name, num_photos, interval)
 #find, open and start low-res camera
 cam_list = pygame.camera.list_cameras()
 webcam = pygame.camera.Camera(cam_list[0],(32,24))
 webcam.start()
 
-#grab image, scale and blit to screen
-imagen = webcam.get_image()
-imagen = pygame.transform.scale(imagen,(640,480))
-pygame.image.save(imagen, "image.jpg")
+for i in range(0, num_photos-1):
+    #grab image, scale and blit to screen
+    imagen = webcam.get_image()
+    #imagen = pygame.transform.scale(imagen,(640,480))
+    pygame.image.save(imagen, file_name+str(i)+"jpg")
+    system.sleep(interval)
+
 webcam.stop()
-sys.exit()
+
