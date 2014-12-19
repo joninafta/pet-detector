d6be1d52554f157d19b3f0fb6a153d2cb8f37405
Jonathan <pi@raspbmc.(none)>
2014-12-19 19:03:43 +0200

    fix

--- camera_scripts.py
+++ camera_scripts.py
@@ -4,10 +4,10 @@ import time
 import subprocess
 #pygame.camera.init()
 
-camera = picamera.PiCamera()
-camera.resolution = (1024, 768)
-camera.rotation = 180
-camera.quality = 15
+#camera = picamera.PiCamera()
+#camera.resolution = (1024, 768)
+#camera.rotation = 180
+#camera.quality = 15
 #camera.led = False
         
 def webcam_take_photo(file_name, num_photos, interval):
