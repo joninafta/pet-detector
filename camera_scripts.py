21dd758bf47f6d35689de52a00d606f5326f895b
Jonathan <yoninafta@gmail.com>
2014-12-19 12:22:43 +0200

    remove webcam support

--- camera_scripts.py
+++ camera_scripts.py
@@ -10,18 +10,18 @@ camera.rotation = 180
 camera.quality = 15
 #camera.led = False
         
-def webcam_take_photo(file_name, num_photos, interval):
-    #find, open and start low-res camera
-    cam_list = pygame.camera.list_cameras()
-    webcam = pygame.camera.Camera(cam_list[0],(640,480))
-    webcam.start()
-    for i in range(1, num_photos+1):
-        #grab image, scale and blit to screen
-        imagen = webcam.get_image()
-        pygame.image.save(imagen, file_name+str(i)+".jpg")
-        time.sleep(interval)
-    webcam.stop()
-    
+##def webcam_take_photo(file_name, num_photos, interval):
+##    #find, open and start low-res camera
+##    cam_list = pygame.camera.list_cameras()
+##    webcam = pygame.camera.Camera(cam_list[0],(640,480))
+##    webcam.start()
+##    for i in range(1, num_photos+1):
+##        #grab image, scale and blit to screen
+##        imagen = webcam.get_image()
+##        pygame.image.save(imagen, file_name+str(i)+".jpg")
+##        time.sleep(interval)
+##    webcam.stop()
+##    
 def picamera_take_photo(file_name, num_photos, interval):
     for i in range(1, num_photos+1):
         camera.capture(file_name+str(i)+".jpg",use_video_port=False)
